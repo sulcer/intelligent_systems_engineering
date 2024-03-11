ec6778facc88835ea9c6970b17464e0125279ba8
sulcer <gregor.sulcer@student.um.si>
2024-03-11 10:55:28 +0100

    feat: model train script

---
+++ src/models/train_model.py
@@ -0,0 +1,29 @@
+import joblib
+import pandas as pd
+from sklearn.preprocessing import MinMaxScaler
+from src.models.utils.utils import create_test_train_split, create_time_series, create_model
+
+if __name__ == "__main__":
+    data = pd.read_csv("../../data/processed/mbajk_dataset.csv")
+    data.drop(columns=["date"], inplace=True)
+
+    target_variable = "available_bike_stands"
+    features = [target_variable] + [col for col in data.columns if col != target_variable]
+    data = data[features]
+
+    train_data, test_data = create_test_train_split(data)
+
+    scaler = MinMaxScaler()
+    train_data = scaler.fit_transform(train_data)
+    test_data = scaler.transform(test_data)
+
+    window_size = 50
+
+    X_train, y_train = create_time_series(train_data, window_size)
+    X_test, y_test = create_time_series(test_data, window_size)
+
+    model = create_model(X_train)
+    model.fit(X_train, y_train, epochs=10, batch_size=64, validation_data=(X_test, y_test), verbose=1)
+
+    joblib.dump(scaler, "../models/min_max_scaler.gz")
+    model.save(f"../models/mbajk_{model.name}.keras")
---
+++ src/models/utils/utils.py
@@ -0,0 +1,39 @@
+from typing import Tuple
+import numpy as np
+import pandas as pd
+from keras import Sequential, Input
+from keras.src.layers import GRU, Dropout, Dense
+from keras.src.optimizers import Adam
+
+
+def create_test_train_split(dataset: pd.DataFrame) -> Tuple[pd.DataFrame, pd.DataFrame]:
+    test_data_size = round(0.2 * len(dataset))
+    train_data = dataset[:-test_data_size]
+    test_data = dataset[-test_data_size:]
+
+    return train_data, test_data
+
+
+def create_time_series(data, n_past):
+    X, y = [], []
+    for i in range(n_past, len(data)):
+        X.append(data[i - n_past:i, 0:data.shape[1]])
+        y.append(data[i, 0])
+    return np.array(X), np.array(y)
+
+
+def create_model(input_shape):
+    model = Sequential(name='GRU')
+
+    model.add(Input(shape=(input_shape.shape[1], input_shape.shape[2])))
+    model.add(GRU(128, return_sequences=True))
+    model.add(Dropout(0.2))
+    model.add(GRU(64, return_sequences=True))
+    model.add(Dropout(0.2))
+    model.add(GRU(32))
+    model.add(Dense(units=32, activation="relu"))
+    model.add(Dense(1))
+
+    optimizer = Adam(learning_rate=0.01)
+    model.compile(optimizer=optimizer, loss="mean_squared_error")
+    return model
