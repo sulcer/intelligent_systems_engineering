0fdee547f3099b083c8753a5437f6dc865dc2fa8
sulcer <gregor.sulcer@student.um.si>
2024-03-16 23:11:41 +0100

    fix: tests

--- src/serve/app/main.py
+++ src/serve/app/main.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI
 from starlette.middleware.cors import CORSMiddleware
-from .routers import health
+from starlette.responses import RedirectResponse
+from .routers import health_router, prediction_router
 
 app = FastAPI()
 
@@ -14,4 +15,10 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-app.include_router(health.router)
+app.include_router(health_router)
+app.include_router(prediction_router)
+
+
+@app.get("/")
+def root():
+    return RedirectResponse(url="/docs")
--- src/serve/app/routers/__init__.py
+++ src/serve/app/routers/__init__.py
@@ -1,3 +1,4 @@
-from . import health, prediction
+from .health import router as health_router
+from .prediction import router as prediction_router
 
-__all__ = ['health', 'prediction']
+__all__ = ['health_router', 'prediction_router']
