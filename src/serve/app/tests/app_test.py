a5f594b1ecc4da65a26965639d6218d6d48145f5
sulcer <gregor.sulcer@student.um.si>
2024-04-08 16:03:15 +0200

    feat: dvc setup

--- src/serve/app/tests/app_test.py
+++ src/serve/app/tests/app_test.py
@@ -1,2 +1,15 @@
-def test_dummy():
-    assert 1 == 1
+from fastapi.testclient import TestClient
+from src.serve.app.main import app
+
+client = TestClient(app)
+
+
+def test_health_check():
+    response = client.get("/health")
+    assert response.status_code == 200
+    assert "status" in response.json()
+
+
+def test_predict_multiple():
+    response = client.get("/mbajk/predict/")
+    assert response.status_code == 200
