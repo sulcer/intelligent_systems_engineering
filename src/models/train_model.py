013a65713bc0a0123ebf45937fd37f87a9826cbf
sulcer <gregor.sulcer@student.um.si>
2024-04-30 13:34:29 +0200

    feat: mlflow train tracking

--- src/models/train_model.py
+++ src/models/train_model.py
@@ -4,6 +4,9 @@ import pandas as pd
 from sklearn.preprocessing import MinMaxScaler
 from src.config import settings
 from src.models.utils.utils import create_test_train_split, create_time_series, create_model, run_sklearn_pipeline
+import dagshub
+from dagshub.data_engine.datasources import mlflow
+import dagshub.auth as dh_auth
 
 
 def train_model(station_number: int) -> None:
@@ -25,7 +28,10 @@ def train_model(station_number: int) -> None:
 
     model = create_model(X_train)
 
-    model.fit(X_train, y_train, epochs=10, batch_size=64, validation_data=(X_test, y_test), verbose=1)
+    epochs = 10
+    batch_size = 64
+
+    model.fit(X_train, y_train, epochs=epochs, batch_size=batch_size, validation_data=(X_test, y_test), verbose=1)
 
     if not os.path.exists(f"models/station_{station_number}"):
         os.makedirs(f"models/station_{station_number}")
@@ -33,10 +39,24 @@ def train_model(station_number: int) -> None:
     joblib.dump(scaler, f"models/station_{station_number}/scaler.gz")
     model.save(f"models/station_{station_number}/model.h5")
 
+    mlflow.start_run(run_name=f"train_station_{station_number}")
+    mlflow.log_param("window_size", window_size)
+    mlflow.log_param("batch_size", batch_size)
+    mlflow.log_param("epochs", epochs)
+
+    mlflow.end_run()
+
     print(f"Model for station {station_number} trained")
 
 
 if __name__ == "__main__":
+    dh_auth.add_app_token(token=settings.dagshub_user_token)
+    dagshub.init(settings.dagshub_repo_name, settings.mlflow_tracking_username, mlflow=True)
+    mlflow.set_tracking_uri(settings.mlflow_tracking_uri)
+
+    if mlflow.active_run():
+        mlflow.end_run()
+
     dir_path = "data/processed/"
     for file in os.listdir(dir_path):
         if file.startswith("station_") and file.endswith(".csv"):
