568a9ba14a16d09af25eaaa5f945078a85a4658b
sulcer <gregor.sulcer@student.um.si>
2024-05-03 13:06:52 +0200

    chore: refactor prediction validation code

--- src/data/validation/validate_predictions.py
+++ src/data/validation/validate_predictions.py
@@ -6,18 +6,15 @@ import pandas as pd
 from sqlalchemy.orm import sessionmaker
 from src.serve.database import create_database_engine
 from src.serve.models.prediction import Prediction
-# from dagshub.data_engine.datasources import mlflow
-# import dagshub.auth as dh_auth
-# from sqlalchemy.orm import sessionmaker
-# from src.config import settings
-# from src.serve.database import create_database_engine
-# from src.serve.models.prediction import Prediction
+from dagshub.data_engine.datasources import mlflow
+import dagshub.auth as dh_auth
+from src.config import settings
 
 
 def main():
-    # dh_auth.add_app_token(token=settings.dagshub_user_token)
-    # dagshub.init(settings.dagshub_repo_name, settings.mlflow_tracking_username, mlflow=True)
-    # mlflow.set_tracking_uri(settings.mlflow_tracking_uri)
+    dh_auth.add_app_token(token=settings.dagshub_user_token)
+    dagshub.init(settings.dagshub_repo_name, settings.mlflow_tracking_username, mlflow=True)
+    mlflow.set_tracking_uri(settings.mlflow_tracking_uri)
 
     engine = create_database_engine()
     Session = sessionmaker(bind=engine)
@@ -25,15 +22,13 @@ def main():
 
     all_predictions = session.query(Prediction).all()
 
-    print(all_predictions[-1].predictions)
-
     dir_path = "data/processed/"
 
     station_numbers = [file.split("_")[1].split(".")[0] for file in os.listdir(dir_path) if file.startswith("station")]
 
     for station_number in station_numbers:
         print(f"Validating predictions for station {station_number}")
-        # mlflow.start_run(run_name=f"validate_predictions_mbajk_station_{station_number}")
+        mlflow.start_run(run_name=f"validate_predictions_mbajk_station_{station_number}")
 
         station_data = pd.read_csv(f"data/processed/station_{station_number}.csv")
         station_data = bind_latest_fetch_timestamp(station_data)
@@ -63,9 +58,9 @@ def main():
         mean_squared_error = np.mean(np.square(errors))
         print(f"Mean squared error for station {station_number}: {mean_squared_error}")
 
-        # mlflow.log_metric("mean_error", mean_error)
-        # mlflow.log_metric("mean_squared_error", mean_squared_error)
-        # mlflow.end_run()
+        mlflow.log_metric("mean_error", mean_error)
+        mlflow.log_metric("mean_squared_error", mean_squared_error)
+        mlflow.end_run()
 
 
 def bind_latest_fetch_timestamp(station_data: pd.DataFrame) -> pd.DataFrame:
