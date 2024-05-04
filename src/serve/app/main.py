fc3948216e2563ced7fcdc79b04684197db0cbcd
sulcer <gregor.sulcer@student.um.si>
2024-05-04 12:36:55 +0200

    fix: model registry

--- src/serve/app/main.py
+++ src/serve/app/main.py
@@ -2,6 +2,7 @@ from fastapi import FastAPI
 from starlette.middleware.cors import CORSMiddleware
 from starlette.responses import RedirectResponse
 from .routers import health_router, prediction_router, bike_stations_router
+from ...models.model_registry import download_model_registry
 
 app = FastAPI()
 
@@ -19,6 +20,11 @@ app.include_router(health_router)
 app.include_router(prediction_router)
 app.include_router(bike_stations_router)
 
+
+# download model registry
+@app.on_event("startup")
+async def startup_event():
+    download_model_registry()
 
 @app.get("/")
 def root():
