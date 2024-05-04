b19fcda4efa8ec03c9b034f70c8bf8f003bfffd5
sulcer <gregor.sulcer@student.um.si>
2024-05-04 22:07:36 +0200

    feat: ui header

--- src/client/components/map/bike-stations-map.tsx
+++ src/client/components/map/bike-stations-map.tsx
@@ -4,6 +4,7 @@ import {APIProvider, Map} from '@vis.gl/react-google-maps';
 import {useRouter} from "next/navigation";
 import {useBikeStations} from "@/lib/hooks/bike-stations";
 import PredictionDialog from "@/components/prediction/prediction-dialog";
+import Header from "@/components/ui/Header";
 
 const BikeStationsMap = () => {
     const router = useRouter();
@@ -11,6 +12,7 @@ const BikeStationsMap = () => {
 
     return (
         <>
+            <Header />
             <APIProvider apiKey={process.env.NEXT_PUBLIC_GOOGLE_MAPS_KEY as string}>
                 <Map
                     style={{width: '100vw', height: '100vh'}}
---
+++ src/client/components/ui/Header.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+        <div className="flex justify-center">
+            <h1 className="text-2xl font-bold text-center absolute z-10 my-3 px-6 p-3 bg-[#E73439] rounded-full text-white shadow-xl">
+                Intelligent Systems Engineering for Mbajk
+            </h1>
+        </div>
+    );
+};
+
+export default Header;
